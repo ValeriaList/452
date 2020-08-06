12a7a5d9156b1d930b15f94f45d9fea7f1aa0b71
Валерия <69154310+ValeriaList@users.noreply.github.com>
2020-08-06 15:56:55 +0300

    Добавьте файлы проекта.

---
+++ пробник/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace пробник
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
